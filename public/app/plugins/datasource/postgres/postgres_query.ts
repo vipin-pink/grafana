d3c65cbc9e2637ebeca1a7f897cbed103efa3681
Vipin Tanna <vipin.tanna@pinkroccade.nl>
2018-03-05 17:53:36 +0100

    feat: query builder integration for grafana 4.6.x

---
+++ public/app/plugins/datasource/postgres/postgres_query.ts
@@ -0,0 +1,267 @@
+import _ from 'lodash';
+import queryPart from './query_part';
+import kbn from 'app/core/utils/kbn';
+
+export default class PostgresQuery {
+  target: any;
+  selectModels: any[];
+  queryBuilder: any;
+  groupByParts: any;
+  templateSrv: any;
+  scopedVars: any;
+
+  /** @ngInject */
+  constructor(target, templateSrv?, scopedVars?) {
+    this.target = target;
+    this.templateSrv = templateSrv;
+    this.scopedVars = scopedVars;
+
+    target.schema = target.schema || 'public';
+    target.format = target.format || 'time_series';
+    target.timeColumn = target.timeColumn || 'time';
+    target.metricColumn = target.metricColumn || 'None';
+
+    target.orderByTime = target.orderByTime || 'ASC';
+    target.groupBy = target.groupBy || [];
+    target.where = target.where || [];
+    target.select = target.select || [[{ type: 'column', params: ['value'] }]];
+
+    this.updateProjection();
+  }
+
+  quoteIdentifier(value) {
+    return '"' + value + '"';
+  }
+
+  quoteLiteral(value) {
+    return "'" + value + "'";
+  }
+
+  updateProjection() {
+    this.selectModels = _.map(this.target.select, function(parts: any) {
+      return _.map(parts, queryPart.create);
+    });
+    this.groupByParts = _.map(this.target.groupBy, queryPart.create);
+  }
+
+  updatePersistedParts() {
+    this.target.select = _.map(this.selectModels, function(selectParts) {
+      return _.map(selectParts, function(part: any) {
+        return { type: part.def.type, params: part.params };
+      });
+    });
+  }
+
+  hasGroupByTime() {
+    return _.find(this.target.groupBy, (g: any) => g.type === 'time');
+  }
+
+  hasFill() {
+    return _.find(this.target.groupBy, (g: any) => g.type === 'fill');
+  }
+
+  addGroupBy(value) {
+    var stringParts = value.match(/^(\w+)(\((.*)\))?$/);
+    var typePart = stringParts[1];
+    var args = stringParts[3].split(',');
+    var partModel = queryPart.create({ type: typePart, params: args });
+    var partCount = this.target.groupBy.length;
+
+    if (partCount === 0) {
+      this.target.groupBy.push(partModel.part);
+    } else if (typePart === 'time') {
+      this.target.groupBy.splice(0, 0, partModel.part);
+    } else if (typePart === 'column') {
+      if (this.target.groupBy[partCount - 1].type === 'fill') {
+        this.target.groupBy.splice(partCount - 1, 0, partModel.part);
+      } else {
+        this.target.groupBy.push(partModel.part);
+      }
+    } else {
+      this.target.groupBy.push(partModel.part);
+    }
+
+    this.updateProjection();
+  }
+
+  removeGroupByPart(part, index) {
+    var categories = queryPart.getCategories();
+
+    if (part.def.type === 'time') {
+      // remove aggregations
+      this.target.select = _.map(this.target.select, (s: any) => {
+        return _.filter(s, (part: any) => {
+          var partModel = queryPart.create(part);
+          if (partModel.def.category === categories.Aggregations) {
+            return false;
+          }
+          return true;
+        });
+      });
+    }
+
+    this.target.groupBy.splice(index, 1);
+    this.updateProjection();
+  }
+
+  removeSelect(index: number) {
+    this.target.select.splice(index, 1);
+    this.updateProjection();
+  }
+
+  removeSelectPart(selectParts, part) {
+    // if we remove the field remove the whole statement
+    if (part.def.type === 'column') {
+      if (this.selectModels.length > 1) {
+        var modelsIndex = _.indexOf(this.selectModels, selectParts);
+        this.selectModels.splice(modelsIndex, 1);
+      }
+    } else {
+      var partIndex = _.indexOf(selectParts, part);
+      selectParts.splice(partIndex, 1);
+    }
+
+    this.updatePersistedParts();
+  }
+
+  addSelectPart(selectParts, type) {
+    var partModel = queryPart.create({ type: type });
+    partModel.def.addStrategy(selectParts, partModel, this);
+    this.updatePersistedParts();
+  }
+
+  private renderTagCondition(tag, index, interpolate) {
+    var str = '';
+    var operator = tag.operator;
+    var value = tag.value;
+    if (index > 0) {
+      str = (tag.condition || 'AND') + ' ';
+    }
+
+    if (!operator) {
+      if (/^\/.*\/$/.test(value)) {
+        operator = '=~';
+      } else {
+        operator = '=';
+      }
+    }
+
+    // quote value unless regex
+    if (operator !== '=~' && operator !== '!~') {
+      if (interpolate) {
+        value = this.templateSrv.replace(value, this.scopedVars);
+      }
+      if (operator !== '>' && operator !== '<') {
+        value = "'" + value.replace(/\\/g, '\\\\') + "'";
+      }
+    } else if (interpolate) {
+      value = this.templateSrv.replace(value, this.scopedVars, 'regex');
+    }
+
+    return str + '"' + tag.key + '" ' + operator + ' ' + value;
+  }
+
+  interpolateQueryStr(value, variable, defaultFormatFn) {
+    // if no multi or include all do not regexEscape
+    if (!variable.multi && !variable.includeAll) {
+      return value;
+    }
+
+    if (typeof value === 'string') {
+      return kbn.regexEscape(value);
+    }
+
+    var escapedValues = _.map(value, kbn.regexEscape);
+    return '(' + escapedValues.join('|') + ')';
+  }
+
+  render(interpolate?) {
+    var target = this.target;
+
+    if (target.rawQuery) {
+      if (interpolate) {
+        return this.templateSrv.replace(target.rawSql, this.scopedVars, this.interpolateQueryStr);
+      } else {
+        return target.rawSql;
+      }
+    }
+
+    var query = 'SELECT ';
+
+    var timeGroup = this.hasGroupByTime();
+
+    if (timeGroup) {
+      var args;
+      if (timeGroup.params.length > 1 && timeGroup.params[1] !== 'none') {
+        args = timeGroup.params.join(',');
+      } else {
+        args = timeGroup.params[0];
+      }
+      query += '$__timeGroup(' + this.quoteIdentifier(target.timeColumn) + ',' + args + '),';
+    } else {
+      query += this.quoteIdentifier(target.timeColumn) + ' AS time,';
+    }
+
+    var i, y;
+    for (i = 0; i < this.selectModels.length; i++) {
+      let parts = this.selectModels[i];
+      var selectText = '';
+      for (y = 0; y < parts.length; y++) {
+        let part = parts[y];
+        selectText = part.render(selectText);
+      }
+
+      if (i > 0) {
+        query += ', ';
+      }
+      query += selectText;
+    }
+
+    if (this.target.metricColumn !== 'None') {
+      query += ',' + this.quoteIdentifier(this.target.metricColumn) + ' AS metric';
+    }
+
+    query += ' FROM ' + target.schema + '.' + target.table + ' WHERE ';
+    var conditions = _.map(target.where, (tag, index) => {
+      return this.renderTagCondition(tag, index, interpolate);
+    });
+
+    if (conditions.length > 0) {
+      query += '(' + conditions.join(' ') + ') AND ';
+    }
+
+    query += '$__timeFilter(' + this.quoteIdentifier(target.timeColumn) + ')';
+
+    var groupBySection = '';
+    for (i = 0; i < this.groupByParts.length; i++) {
+      var part = this.groupByParts[i];
+      if (i > 0) {
+        groupBySection += ', ';
+      }
+      if (part.def.type === 'time') {
+        groupBySection += '1';
+      } else {
+        groupBySection += part.render('');
+      }
+    }
+
+    if (groupBySection.length) {
+      query += ' GROUP BY ' + groupBySection;
+    }
+
+    query += ' ORDER BY 1';
+
+    this.target.rawSql = query;
+    if (interpolate) {
+      query = this.templateSrv.replace(query, this.scopedVars, this.interpolateQueryStr);
+    }
+    return query;
+  }
+
+  renderAdhocFilters(filters) {
+    var conditions = _.map(filters, (tag, index) => {
+      return this.renderTagCondition(tag, index, false);
+    });
+    return conditions.join(' ');
+  }
+}
